2bc82e3dd296b8c4a9a1038ea37886df8a143bc1
Sam Yao <yaosam.521@gmail.com>
2023-11-07 16:04:51 -0600

    Initial Commit

---
+++ CS5323_MLaaS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS5323_MLaaS
+//
+//  Created by Sam Yao on 11/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CS5323_MLaaSUITests/CS5323_MLaaSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CS5323_MLaaSUITestsLaunchTests.swift
+//  CS5323_MLaaSUITests
+//
+//  Created by Sam Yao on 11/7/23.
+//
+
+import XCTest
+
+final class CS5323_MLaaSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
