974e1d58219e8a7debbf99ed4c761454150c562b
Larry Y <larryyu0116@gmail.com>
2023-11-16 16:19:12 -0600

    record and play

--- CS5323_MLaaS/ModuleAViewController.swift
+++ CS5323_MLaaS/ModuleAViewController.swift
@@ -6,22 +6,78 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ModuleAViewController: UIViewController {
+class ModuleAViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var postButton: UIButton!
     
-
-
+    var soundRecorder = AVAudioRecorder()
+    var soundPlayer = AVAudioPlayer()
+    var fileName = "audioFile.m4a"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    @IBAction func recordButtonAction(_ sender: Any) {
-    }
-    
-    @IBAction func sendAudioSample(_ sender: Any) {
+        setupRecorder()
     }
     
+    func setupRecorder() {
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [])
+            try audioSession.setActive(true)
+            
+            let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let soundFilePath = documentPath.appendingPathComponent(fileName)
+            
+            let recordSettings: [String: Any] = [
+                AVFormatIDKey: kAudioFormatAppleLossless,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue,
+                AVEncoderBitRateKey: 320000,
+                AVSampleRateKey: 44100.0,
+                AVNumberOfChannelsKey: 2,
+            ]
+            
+            soundRecorder = try AVAudioRecorder(url: soundFilePath, settings: recordSettings)
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        } catch {
+            print("Error setting up audio session or recorder: \(error.localizedDescription)")
+        }
+    }
+                                        
+    
+    @IBAction func recordSound(_ sender: Any) {
+        if !soundRecorder.isRecording {
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+                soundRecorder.record()
+            } catch {
+                print("Error starting recording: \(error.localizedDescription)")
+            }
+        } else {
+            soundRecorder.stop()
+        }
+    }
+    
+    @IBAction func playSound(_ sender: Any) {
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: soundRecorder.url)
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+    
+    @IBAction func postSound(_ sender: Any) {
+        
+    }
     /*
     // MARK: - Navigation
 
